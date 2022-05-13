8777439541465c0893182a3aeeff8853d0b74ab2
koala117 <cassieralexis@gmail.com>
2022-05-13 10:34:38 +0200

    add css

--- src/component/Login/LoginBlock..js
+++ src/component/Login/LoginBlock..js
@@ -6,27 +6,30 @@ import './style.css';
 function LoginBlock() {
     return (
         <Card className="p-3">
-            <Card.Title className='text-center'>Coucou toi</Card.Title>
+            <Card.Title className='text-center text-form'>Coucou toi</Card.Title>
             <Card.Body>
                 <Form>
                     <Form.Group>
-                        <Form.Text width="50%" muted>
+                        <Form.Text width="50%" muted className="text-form">
                             Alors comme ca, on a une petite liste a préparer
                         </Form.Text>
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="formBasicEmail">
-                        <Form.Label htmlFor="inputNomUtilisateur">Adresse Email</Form.Label>
-                        <Form.Control type="email"/>
+                        <Form.Label htmlFor="inputNomUtilisateur" className="text-form">Adresse Email</Form.Label>
+                        <Form.Control type="email" className="text-area"/>
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="formBasicPassword">
-                        <Form.Label htmlFor="inputNomUtilisateur">Mot de passe</Form.Label>
-                        <Form.Control type="password"/>
+                        <Form.Label htmlFor="inputNomUtilisateur" className="text-form">Mot de passe</Form.Label>
+                        <Form.Control type="password" className="text-area"/>
+                    </Form.Group>
+                    <Form.Group>
+                        <div className="text-form">oups , j’ai oublié mon mot de passe</div>
                     </Form.Group>
                 </Form>
             </Card.Body>
             <div className='d-flex justify-content-center'>
-                <Button variant="primary" type="submit">
-                    Envoyer
+                <Button className="btn-submit" variant="primary" type="submit">
+                    Se connecter
                 </Button>
             </div>
         </Card>)
