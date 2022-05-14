6fac5f1c771e861da79f7e0abba359aac0603c54
Mickael-DP <m.dallepasqualine@gmail.com>
2022-05-14 18:28:38 +0200

    finish add header & nav

--- src/component/Navbar/NavVertical.js
+++ src/component/Navbar/NavVertical.js
@@ -1,18 +1,18 @@
 import {Container, Nav, Navbar, NavDropdown} from "react-bootstrap";
 import './style.css';
+import ImageProfil from "../ImageProfil/ImageProfil";
 
 function NavVertical() {
     return (
         <div className='Navbar-vertical'>
             <Navbar bg="dark" expand="lg">
-                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav"/>
-                <Navbar.Collapse>
+                <Navbar.Brand href="#home"> <ImageProfil /></Navbar.Brand>
+                <Navbar>
                     <Nav className="me-auto">
                         <Nav.Link href="#home">Créer une liste</Nav.Link>
                         <Nav.Link href="#link">Mes listes</Nav.Link>
                     </Nav>
-                </Navbar.Collapse>
+                </Navbar>
             </Navbar>
         </div>
 
