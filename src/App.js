6b26f812480c4f1b4d5a10e04ec080f78eb3104d
Mickael-DP <m.dallepasqualine@gmail.com>
2022-05-20 10:06:29 +0200

    modif app.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
-import logo from './logo.svg';
+
 import './App.css';
-import Login from "./Login/Login";
 import Header from "./component/Header/Header";
-import NavVertical from "./component/Navbar/NavVertical";
-import PageList from "./PageList/PageList";
+
 
 function App() {
     return (
         <>
             <Header/>
             <div className='page'>
-                <NavVertical/>
-                <PageList/>
+
             </div>
         </>
 
