09ed85e68b7cd60040fa5dc4aa3abbccf066e84b
Mickael-DP <m.dallepasqualine@gmail.com>
2022-05-20 15:04:18 +0200

    install routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Login from "./Login/Login";
+import PageList from "./PageList/PageList";
+import PageCreateList from "./PageCreateList/PageCreateList";
 
 
 function App() {
     return (
-      <>
-
-      </>
+        <BrowserRouter>
+            <Routes>
+                <Route path= "/" element={<Login />} />
+                <Route path="/list" element={<PageList />}/>
+                <Route path="/createlist" element={<PageCreateList/>}/>
+            </Routes>
+        </BrowserRouter>
     );
 }
 
